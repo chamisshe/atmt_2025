95b98260bcaa15468dd6b89c9cd23512e77a3c10
Mingqi Chen <zhocasserole@outlook.com>
2025-11-10 00:35:41 +0100

    sync assignment 1 changes

---
+++ assignment1_translate.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash -l
+#SBATCH --partition teaching
+#SBATCH --time=24:0:0
+#SBATCH --ntasks=1
+#SBATCH --mem=16GB
+#SBATCH --cpus-per-task=1
+#SBATCH --gpus=1
+#SBATCH --output=out_assignment1_translate.out
+
+module load gpu
+module load mamba
+source activate atmt
+export XLA_FLAGS=--xla_gpu_cuda_data_dir=$CONDA_PREFIX/pkgs/cuda-toolkit
+
+
+
+# TRANSLATE
+python translate.py \
+    --cuda \
+    --input ~/shares/cz-en/data/raw/test.cz \
+    --src-tokenizer cz-en/tokenizers/cz-bpe-8000.model \
+    --tgt-tokenizer cz-en/tokenizers/en-bpe-8000.model \
+    --checkpoint-path cz-en/checkpoints/checkpoint_best.pt \
+    --output cz-en/output.txt \
+    --bleu \
+    --reference ~/shares/cz-en/data/raw/test.en \
+    --max-len 300
